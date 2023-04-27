f589a7f71b901c53897e29cd3e8285e1f2198c8e
alexsogoat <zmxnccv@gmail.com>
2023-04-27 15:10:36 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/company/customview/CustomView.java
@@ -0,0 +1,27 @@
+package kr.co.company.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+        setBackgroundColor(Color.YELLOW);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context);
+        setBackgroundColor(Color.YELLOW);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawArc(new RectF(10, 10, 600, 600), 45, 320, true, paint);
+    }
+}
